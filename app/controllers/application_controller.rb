8b4f5cb2506b36a81e714090ebb02f8f29c44f42
Neil Ryan <neilmarkoliverryan@gmail.com>
2020-12-30 23:40:48 -0800

    Added helper methods  and edit photo and bio feature to show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,52 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "extra_secret"
   end
 
   get "/" do
-    erb :welcome
+    erb :home
+  end
+
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+
+  end
+
+  get "/signup" do
+    erb :'users/create_user'
+  end
+
+  post "/signup" do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect '/signup'
+    else
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect "users/#{@user.slug}"
+    end
+  end
+
+  get "/login" do
+    erb :'users/login'
+  end
+
+  post "/login" do
+    @user = User.find_by(:email => params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      erb :'users/show'
+    else
+      redirect "/login"
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,41 @@
 class UsersController < ApplicationController
 
-  get "/login" do
-    erb :'users/login'
+  #route to find a user's show page by slug
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
   end
 
-  post "/login" do
-    @user = User.find_by(:email => params[:email])
-    if @user && @user.authenticate(params[:password])
-      erb :'users/show'
-    else
-      redirect "/login"
+  get '/profile' do
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      erb :'users/profile_pic'
     end
   end
+
+  post '/profile' do
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      @user.image_url = params[:image_url]
+      @user.save
+      redirect "users/#{@user.slug}"
+    end
+  end
+
+  get '/bio' do
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      erb :'users/bio'
+    end
+  end
+
+  post '/bio' do
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      @user.bio = params[:bio]
+      @user.save
+      erb :'users/show'
+    end
+  end
+
 end
