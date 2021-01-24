8283ae5d6c6da985fcdb624b10c5b19e54b1bd61
Neil Ryan <neilmarkoliverryan@gmail.com>
2021-01-23 16:46:46 -0800

    changed data protection conditionns from patch controllers to edit controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
 
+  #route to render a new item form
   get '/items/new' do
     if logged_in?
       erb :'items/new'
@@ -8,46 +9,50 @@ class ItemsController < ApplicationController
     end
   end
 
+  #route to create a new item using params
   post '/items' do
-    #binding.pry
     if logged_in?
       @user = current_user
       @item = Item.new(name: params[:name], description: params[:description], weight: params[:weight], image_url: params[:image_url])
       @user.items.push(@item)
       @user.save
-      #binding.pry
       redirect "users/#{@user.slug}"
     else
       redirect '/login'
     end
   end
 
+  #route to load all users items
   get '/items/index' do
     @items = Item.all
     erb :'items/index'
   end
 
+  #route to load an item by id
   get '/items/:id' do
     @item = Item.find(params[:id])
     erb :'/items/show'
   end
 
+  #route to render an edit form for item
   get '/items/:id/edit' do
     @item = Item.find_by(id: params[:id])
-    erb :'items/edit'
-  end
-
-  patch '/items/:id' do
-    @item = Item.find_by(id: params[:id])
     if current_user == @item.user
-      @item.update(name: params[:name], description: params[:description], weight: params[:weight], image_url: params[:image_url])
-      redirect "/items/#{@item.id}"
+      erb :'items/edit'
     else
       flash[:message] = "Sorry, \nyou can't edit an item you did not post."
       redirect "/items/#{@item.id}"
     end
   end
 
+  #route to update an item
+  patch '/items/:id' do
+    @item = Item.find_by(id: params[:id])
+    @item.update(name: params[:name], description: params[:description], weight: params[:weight], image_url: params[:image_url])
+    redirect "/items/#{@item.id}"
+  end
+
+  #route to delete an item
   delete '/items/:id/delete' do
     @item = Item.find_by(id: params[:id])
     if current_user == @item.user
--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -1,5 +1,6 @@
 class PacksController < ApplicationController
 
+  #route to render form to create new pack
   get '/packs/new' do
     if logged_in?
       @items = current_user.items
@@ -9,6 +10,7 @@ class PacksController < ApplicationController
     end
   end
 
+  #route to render form to create new pack
   post '/packs' do
     if logged_in?
       @user = current_user
@@ -22,8 +24,7 @@ class PacksController < ApplicationController
 
       #convert quantity array to string, store it in the db and make instance variable in '/packs/:id'
       @quantity_items = params[:quantity][:items]
-      @pack.quantity_string = @quantity_items.join(",");
-
+      @pack.quantity_string = @quantity_items.join(",")
       @user.packs.push(@pack)
       @user.save
       redirect "packs/#{@pack.id}"
@@ -32,11 +33,13 @@ class PacksController < ApplicationController
     end
   end
 
+  #route to load a form to add a blurb
   get '/packs/:id/blurb' do
     @pack = Pack.find_by(id: params[:id])
-    erb :'packs/new_blurb'
+    erb :'packs/blurb'
   end
 
+  #route to create the blurb
   post '/packs/:id/blurb' do
     @pack = Pack.find_by(id: params[:id])
     @pack.blurb = params[:blurb]
@@ -44,11 +47,13 @@ class PacksController < ApplicationController
     redirect "/packs/#{@pack.id}"
   end
 
+  #route to load all packs to show
   get '/packs/index' do
     @packs = Pack.all
     erb :'packs/index'
   end
 
+  #route to load a pack to show by :id
   get '/packs/:id' do
     @pack = Pack.find_by(id: params[:id])
 
@@ -56,6 +61,7 @@ class PacksController < ApplicationController
     if @pack.quantity_string
       @quantity_array = @pack.quantity_string.split(",")
     end
+
     #calculate the total pack weigtht
     @total_pack_grams = 0;
     @pack.user.items.each_with_index do |item, index|
@@ -63,6 +69,7 @@ class PacksController < ApplicationController
         @total_pack_grams += item.weight * @quantity_array[index].to_i;
       end
     end
+
     #convert grams to pounds and ounces formula: lb = g * 0.0022046
     @total_pack_pounds = @total_pack_grams * 0.0022046;
 
@@ -77,48 +84,49 @@ class PacksController < ApplicationController
     erb :'packs/show'
   end
 
+  #route to load a form to edit a pack
   get '/packs/:id/edit' do
     @pack = Pack.find_by(id: params[:id])
-    #grab the quanity string from the db and convert to array
-    if @pack.quantity_string
-      @quantity_array = @pack.quantity_string.split(",")
-    else
-      @quantity_array = [];
-    end
-
-    erb :'packs/edit'
-  end
-
-  patch '/packs/:id' do
-
-    @pack = Pack.find_by(id: params[:id])
     if current_user == @pack.user
-      #bug fix to prevent an error for a non existant item_ids array in the case all items are removed from a pack
-      if !params.keys.include?("item_ids")
-        @pack.items = [];
+    #grab the quanity string from the db and convert to array
+      if @pack.quantity_string
+        @quantity_array = @pack.quantity_string.split(",")
+      else
+        @quantity_array = [];
       end
-
-      @pack.update(trip_name: params[:trip_name], length: params[:length], weather: params[:weather], image_url: params[:image_url], blurb: params[:blurb])
-
-      #find items by id and load them into the packs items array
-      if params.include?("pack")
-        @packed_item_ids = params[:pack][:item_ids]
-        @packed_item_ids.each do |item_id|
-          @pack.items << Item.find_by(id: item_id)
-        end
-      end
-
-      #convert quantity array to string, store it in the db and make instance variable in '/packs/:id'
-      @pack.quantity_string = params[:quantity][:items].join(",");
-      current_user.packs.push(@pack)
-      current_user.save
-      redirect "/packs/#{@pack.id}"
     else
       flash[:message] = "Sorry, \nyou can't edit a pack you did not create."
       redirect "/packs/#{@pack.id}"
     end
+    erb :'packs/edit'
   end
 
+  #route to update a pack
+  patch '/packs/:id' do
+    @pack = Pack.find_by(id: params[:id])
+
+    #bug fix to prevent an error for a non existant item_ids array in the case all items are removed from a pack
+    if !params.keys.include?("item_ids")
+      @pack.items = [];
+    end
+    @pack.update(trip_name: params[:trip_name], length: params[:length], weather: params[:weather], image_url: params[:image_url], blurb: params[:blurb])
+
+    #find items by id and load them into the packs items array
+    if params.include?("pack")
+      @packed_item_ids = params[:pack][:item_ids]
+      @packed_item_ids.each do |item_id|
+        @pack.items << Item.find_by(id: item_id)
+      end
+    end
+
+    #convert quantity array to string, store it in the db and make instance variable in '/packs/:id'
+    @pack.quantity_string = params[:quantity][:items].join(",");
+    current_user.packs.push(@pack)
+    current_user.save
+    redirect "/packs/#{@pack.id}"
+  end
+
+  #route to delete a pack
   delete '/packs/:id/delete' do
     @pack = Pack.find_by(id: params[:id])
     if current_user == @pack.user
